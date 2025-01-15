e60a3762c5c5be05ea4f052d3eee4c21d8c985c5
ai-n0808 <aichan.88.aichan@gmail.com>
2025-01-15 16:49:10 +0900

    feat: add mock data

---
+++ frontend/src/data/menuItems.ts
@@ -0,0 +1,78 @@
+export const menuItem = [
+  {
+    id: 1,
+    name: "Margherita",
+    description:
+      "Italian Bocconcini, Basil Sauce (Half), Basil, Cherry Tomatoes, Tomato Sauce",
+    price: 12.9,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1604/JP_1604_ja_hero_13790.jpg?v1820849339",
+  },
+  {
+    id: 2,
+    name: "Americano",
+    description: "Pepperoni, Tomato Sauce",
+    price: 14.4,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1601/JP_1601_ja_hero_13567.jpg?v974477087",
+  },
+  {
+    id: 3,
+    name: "Tropical",
+    description: "Ham, Pineapple, Tomato Sauce",
+    price: 15.4,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1603/JP_1603_ja_hero_13567.jpg?v1278592244",
+  },
+  {
+    id: 4,
+    name: "Deluxe",
+    description:
+      "Pepperoni, Italian Sausage, Mushrooms, Green Peppers, Onions, Tomato Sauce",
+    price: 19.4,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1602/JP_1602_ja_hero_13567.jpg?v624849729",
+  },
+  {
+    id: 5,
+    name: "Double mozzarella",
+    description: "Mozzarella (Double), Tomato Sauce",
+    price: 17.4,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/2501/JP_2501_ja_hero_13567.jpg?v1110890836",
+  },
+  {
+    id: 6,
+    name: "Giga meat",
+    description:
+      "Pepperoni, Italian Sausage, Pork Sausage, Bacon, Tomato Sauce",
+    price: 22.9,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1610/JP_1610_ja_hero_13567.jpg?v2130784110",
+  },
+  {
+    id: 7,
+    name: "Garlic",
+    description: "Garlic, Pork Sausage, Bacon, Black Pepper, Tomato Sauce",
+    price: 19.9,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1729/JP_1729_ja_hero_13567.jpg?v-1995962369",
+  },
+  {
+    id: 8,
+    name: "Genovese",
+    description: "Ham, Potato Slices, Garlic, Parmesan Mix Cheese, Basil Sauce",
+    price: 19.9,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1606/JP_1606_ja_hero_13567.jpg?v-2069728184",
+  },
+  {
+    id: 9,
+    name: "Mayo potato",
+    description:
+      "Potato Slices, Pork Sausage, Mayo Sauce, Parsley Flakes, Tomato Sauce",
+    price: 18.9,
+    image:
+      "https://www.dominos.jp/ManagedAssets/JP/product/1609/JP_1609_ja_hero_13567.jpg?v-1892676920",
+  },
+];
